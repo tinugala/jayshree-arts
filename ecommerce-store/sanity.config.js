8330ea40ac1c0a45f846e5df8a89b93e481d373f
tinugala <tinugala786@gmail.com>
2023-03-16 17:24:08 +0530

    updated peer dependencies-2

--- ecommerce-store/sanity.config.js
+++ ecommerce-store/sanity.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'sanity'
+import {defineConfig} from 'sanity/cli'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
